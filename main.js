c3fb174a7cd87ed318ae5801ff6f2348f11d6227
Matthias Hryniszak <padcom@gmail.com>
2022-01-25 11:43:50 +0100

    Updated build process to not hot-reload the app (doesn't work with Electron)

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const { app, BrowserWindow } = require('electron')
 const isDev = require("electron-is-dev");
+const fetch = require('electron-fetch').default
 
 const SerialPort = require('serialport')
-console.log(SerialPort.list())
 
-function createWindow () {
+function waitForPageToBuild(timeout = 5000) {
+  const t1 = Date.now()
+  return new Promise(async (resolve, reject) => {
+    while(1) {
+      const t2 = Date.now()
+
+      if (t2 - t1 > timeout) {
+        reject('Timeout')
+        break
+      }
+
+      try {
+        const response = await fetch('http://localhost:8080')
+        if (response.status === 200) {
+          resolve()
+          break;
+        }
+      } catch {
+        // we can't connect in the beginning so we catch and swallow the exception
+      }
+    }
+  })
+}
+
+async function createWindow () {
+  await waitForPageToBuild()
+
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -17,26 +43,31 @@ function createWindow () {
   })
 
   if (isDev) {
-    win.loadURL('http://localhost:1234')
+    win.loadURL('http://localhost:8080')
   } else {
     console.log('This will not work...')
     win.loadFile('index.html')
   }
 }
 
-
-app.whenReady().then(() => {
+async function main() {
+  await app.whenReady()
   app.allowRendererProcessReuse = false
+
+  const ports = await SerialPort.list()
+  console.log('serial ports:', ports)
 
   createWindow()
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
 
-console.log('process.versions', process.versions)
+  console.log('process.versions', process.versions)
+}
+
+main()
